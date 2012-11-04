f48dc17dfb43eabc77d256de4e53c451809c18e3
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-11-04 15:22:39 +0900

    Fix

---
+++ examples/example.php
@@ -0,0 +1,19 @@
+<?php
+require_once dirname(__FILE__) . '/autoload.php';
+
+$queueFactory = new Yuyat_Bulky_QueueFactory(
+    new Yuyat_Bulky_DbAdapter_PdoMysqlAdapter(new PDO (
+        'mysql:dbname=bulky_test;host=localhost',
+        'root'
+    )),
+    1000
+);
+$queue = $queueFactory->createQueue('users', array('name', 'birthday'));
+
+$queue->on('error', function ($records) {
+    echo "Error!", PHP_EOL;
+});
+
+for ($i = 0; $i < 10000; $i++) {
+    $queue->insert(array('Scott Wino Weinrich', '1960-09-29'));
+}

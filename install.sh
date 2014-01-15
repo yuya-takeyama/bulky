0f3f7e807e81b7e1137521df59e6c48edf306d7d
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-01-16 03:08:38 +0900

    Fix SplClassLoader installation

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ fi
 curl https://raw.github.com/yuya-takeyama/venom/master/venom.php > venom.php
 chmod +x venom.php
 ./venom.php
-wget -O ./vendor/SplClassLoader.php https://raw.github.com/gist/221634/2bc31f04b0ed0ef70daab68516c8d17ba0753f5e/SplClassLoader.php
+wget -O ./vendor/SplClassLoader.php https://gist.github.com/jwage/221634/raw/2bc31f04b0ed0ef70daab68516c8d17ba0753f5e/SplClassLoader.php
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__FILE__) . '/../vendor/SplClassLoader/SplClassLoader.php';
+require_once dirname(__FILE__) . '/../vendor/SplClassLoader.php';
 
 $loader = new SplClassLoader('Yuyat', dirname(__FILE__) . '/../src');
 $loader->setNamespaceSeparator('_');
